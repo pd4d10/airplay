4b8d304f71b8e3bd44b3ef53ffd918a0f140fee7
pd4d10 <pd4d10@gmail.com>
2016-12-27 19:33:03 +0800

    Add protocols

--- src/client.js
+++ src/client.js
@@ -1,3 +1,7 @@
+/**
+ * https://nto.github.io/AirPlay.html
+ */
+
 const { parse } = require('querystring')
 const fetch = require('node-fetch')
 
@@ -8,22 +12,49 @@ module.exports = class Request {
     this.fetch = (url, options) => fetch(`http://${ip}:${port}/${url}`, options)
   }
 
+  async post(url) {
+    const res = this.fetch(url, { method: 'POST' })
+    return res.status === 200
+  }
+
+  async getServerInfo() {
+    const res = this.fetch('/server-info')
+    const text = await res.text()
+    return text
+  }
+
   async play({ location, position }) {
     const res = await this.fetch('/play', {
       method: 'POST',
+      headers: {
+        'Content-Type': 'text/parameters',
+      },
       body: `Content-Location: ${location}\nStart-Position: ${position}`,
     })
+    return res.status === 200
+  }
 
-    if (res.status !== 200) {
-      throw new Error('fail')
-    }
+  setPosition(position) {
+    return this.post(`/scrub?position=${position}`)
+  }
 
-    return true
+  setRate(rate) {
+    return this.post(`/rate?value=${rate}`)
+  }
+
+  stop() {
+    return this.post('/stop')
   }
 
   async getPosition() {
     const res = await this.fetch('/scrub')
     const text = await res.text()
     return parse(text, '\n', ' ')
+  }
+
+  async getPlayBackInfo() {
+    const res = await this.fetch('/playback-info')
+    const text = await res.text()
+    return text
   }
 }
